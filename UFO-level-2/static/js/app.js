60fee163258b391f34ab5711ec2678e69c5d74b4
ryanbandrowski <50809219+ryanbandrowski@users.noreply.github.com>
2019-08-29 17:30:02 -0700

    project setup

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,12 @@
+// from data.js
+var tableData = data;
+
+//      datetime: "1/1/2010",
+//      city: "benton",
+//      state: "ar",
+//      country: "us",
+//      shape: "circle",
+//      durationMinutes: "5 mins.",
+//      comments: "4 bright green circles high 
+
+// YOUR CODE HERE!
